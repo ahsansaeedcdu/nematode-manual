e7cb81ecefbb344ea78349a16bcb8ab1c6804e7b
Ahsan Saeed <s377282@students.cdu.edu.au>
2025-06-24 01:12:09 +0930

    initiate react project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <h1>NEMATODE</h1>
+      </header>
+
+      <main className="main">
+        <h2>NEMATODE</h2>
+      </main>
+    </>
+  );
+}
+
+export default App;
