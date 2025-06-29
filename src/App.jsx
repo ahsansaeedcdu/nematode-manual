5ad1a83e95c3911d3368ca919a6daebd6f63beed
Ahsan Saeed <s377282@students.cdu.edu.au>
2025-06-29 22:31:32 +0930

    update readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css';
-
+import SearchBar from './components/SearchBar';
+import AZFilter from './components/AZFilter';
+import { useState } from 'react';
 function App() {
+  const [query, setQuery] = useState('');
+  const [selectedLetter, setSelectedLetter] = useState(null);
   return (
     <>
       <header className="header">
         <h1>NEMATODE</h1>
+        <SearchBar
+          value={query}
+          onChange={setQuery}
+          onClear={() => setQuery('')}
+        />
+        <AZFilter onLetterClick={(letter) => setSelectedLetter(letter)} />
       </header>
 
       <main className="main">
-        <h2>NEMATODE</h2>
+        
       </main>
     </>
   );
