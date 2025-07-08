ee2609238d8c0e9209fda95c9df79d36c6859618
jahidl <s379123@students.cdu.edu.au>
2025-07-09 01:01:31 +0930

    nematode dataset created, nematode list component rendered

---
+++ src/data/nematodeData.js
@@ -0,0 +1,19 @@
+// src/data/nematodeData.js
+export default {
+  A: [
+    { name: 'Acacia rust', count: 235 },
+    { name: 'African tulip tree', count: 148 },
+    {
+      name: 'Ants', count: 235,
+      children: [
+        { name: 'Argentine ant', count: 365 },
+        { name: 'Big-headed ant', count: 361 }
+      ]
+    }
+  ],
+  B: [
+    { name: 'Banana aphid', count: 103 },
+    { name: 'Banana black Sigatoka', count: 235 }
+  ],
+  
+};
