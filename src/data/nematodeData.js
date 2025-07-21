c30dba41bb5f7fa30050d5a29c832ea231794db5
Ahsan Saeed <s377282@students.cdu.edu.au>
2025-07-21 22:19:35 +0930

    fixed conflicts

--- src/data/nematodeData.js
+++ src/data/nematodeData.js
@@ -15,16 +15,5 @@ export default {
     { name: 'Banana aphid', count: 103 },
     { name: 'Banana black Sigatoka', count: 235 }
   ],
-<<<<<<< HEAD
-=======
-  C: [
-    { name: 'Chamaleon ', count: 103 },
-    { name: 'Cats black Sigatoka', count: 235 }
-  ],
-  D: [
-    { name: 'Dolphin aphid', count: 103 },
-    { name: 'Dragon black Sigatoka', count: 235 }
-  ],
->>>>>>> bf4577e9c82e59973c10a714d353526f73ca8107
   
 };
