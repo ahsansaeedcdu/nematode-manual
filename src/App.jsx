944bf36233beaa245b28e2b023940baca8aafec8
Ahsan Saeed <s377282@students.cdu.edu.au>
2025-07-21 22:02:34 +0930

    implement search

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import NematodeMap from './pages/NematodeMap/NematodeMap';
+import NematodeDetail from './pages/NematodeDetail/NematodeDetail';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/nematodes/map" element={<NematodeMap />} />
+      <Route path="/nematode/:name" element={<NematodeDetail />} />
     </Routes>
   );
 }
