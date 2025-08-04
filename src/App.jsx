44060c25b598412c4adf6fd9c07b08614590424e
jahidl <s379123@students.cdu.edu.au>
2025-08-04 21:15:28 +0930

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import NematodeMap from './pages/NematodeMap/NematodeMap';
 import NematodeDetail from './pages/NematodeDetail/NematodeDetail';
+import Navbar from './components/Navbar/navBar';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={ <> <Navbar /><Home /> </>} />
       <Route path="/nematodes/map" element={<NematodeMap />} />
       <Route path="/nematode/:name" element={<NematodeDetail />} />
     </Routes>
