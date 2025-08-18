62a6b5d82e7087d4dd66479750df1945d9f8db3f
Ahsan Saeed <s377282@students.cdu.edu.au>
2025-08-19 01:32:21 +0930

    fix feedback on map preview model

--- src/components/MapPreviewModal/MapPreviewModal.jsx
+++ src/components/MapPreviewModal/MapPreviewModal.jsx
@@ -100,8 +100,8 @@ export default function MapPreviewModal({
                 className="bg-blue-50"
               >
                 <TileLayer
-                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+                  url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
                 />
                 <RecenterAndInvalidate lat={lat} lng={lng} />
                 <Marker position={[lat, lng]}>
